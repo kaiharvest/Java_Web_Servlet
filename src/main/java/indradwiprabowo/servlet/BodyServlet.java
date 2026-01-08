a7c79b0acdc5f480a40f54d877d094fc26ef86da
kaiharvest <indradwiprabowo27@gmail.com>
2026-01-08 10:08:17 +0700

    web servlet

---
+++ src/main/java/indradwiprabowo/servlet/BodyServlet.java
@@ -0,0 +1,27 @@
+package indradwiprabowo.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/body")
+public class BodyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String html = """
+                <html>
+                <body>
+                    <h1>Belajar Web Servlet</h1>
+                </body>
+                </html>
+                """;
+
+        resp.getWriter().println(html);
+    }
+
+}
